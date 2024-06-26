de041eb0db446d0ee0e4b3dcb23fdfb261375bb0
Portego-00 <po93@cornell.edu>
2024-06-25 20:51:19 -0700

    Polish the leaderboard

--- src/screens/LeaderboardScreen/components/ScoreSlate.tsx
+++ src/screens/LeaderboardScreen/components/ScoreSlate.tsx
@@ -1,22 +1,45 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
 
 type ScoreSlateProps = {
   username: string;
   score: number;
+  position: number;
+  profilePicture: string;
 };
 
-const ScoreSlate = ({username, score}: ScoreSlateProps) => {
+const ScoreSlate = ({
+  username,
+  score,
+  position,
+  profilePicture,
+}: ScoreSlateProps) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.usernameText}>{username}</Text>
-      <Text style={styles.scoreText}>{Math.round(score)}</Text>
+      <View style={styles.positionContainer}>
+        <Text style={styles.positionText}>{position}</Text>
+      </View>
+      <Image
+        source={{uri: profilePicture}}
+        style={styles.profileImageContainer}
+      />
+      <View style={styles.usernameContainer}>
+        <Text style={styles.usernameText}>{username}</Text>
+      </View>
+      <View style={styles.scoreContainer}>
+        <Text style={styles.scoreText}>{Math.round(score)}</Text>
+        <Text style={styles.pointsText}> pts</Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: 'center',
+    width: '100%',
+    height: 80,
+    flexDirection: 'row',
     backgroundColor: '#1E2237',
     borderRadius: 8,
     marginBottom: 16,
@@ -38,9 +61,45 @@ const styles = StyleSheet.create({
   },
   scoreText: {
     color: '#D2D5D9',
-    fontSize: 30,
+    fontSize: 20,
     fontFamily: 'Nunito-Bold',
     fontWeight: '900',
+  },
+  positionContainer: {
+    width: '10%',
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 10,
+  },
+  positionText: {
+    color: '#D2D5D9',
+    fontSize: 18,
+    fontFamily: 'Nunito-Bold',
+    fontWeight: '900',
+  },
+  profileImageContainer: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: '#D2D5D9',
+    marginRight: 10,
+  },
+  usernameContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  scoreContainer: {
+    flexDirection: 'row',
+    marginRight: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  pointsText: {
+    color: '#D2D5D9',
+    fontSize: 18,
+    fontFamily: 'Nunito-Regular',
+    fontWeight: '400',
   },
 });
 
--- src/screens/LeaderboardScreen/index.tsx
+++ src/screens/LeaderboardScreen/index.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, StyleSheet, ScrollView} from 'react-native';
+import {View, StyleSheet, ScrollView, Text, Image} from 'react-native';
 import Background from '../../components/Background';
 import {ProcessedSleepData} from '../../utils/types';
 import TopButton from './components/TopButton';
@@ -25,11 +25,19 @@ const LeaderboardScreen = ({processedSleepData}: LeaderboardScreenProps) => {
     0,
   );
 
-  const {randomNames, randomScores} = generateRandomNamesAndScores();
+  const {randomNames, randomScores} =
+    generateRandomNamesAndScores();
+
 
   const sortedScores = randomNames
-    .map((name, index) => ({name, score: randomScores[index]}))
-    .concat({name: 'You', score: totalWeekScore})
+    .map((name, index) => ({
+      name,
+      score: randomScores[index],
+    }))
+    .concat({
+      name: 'You',
+      score: totalWeekScore,
+    })
     .sort((a, b) => b.score - a.score);
 
   return (
@@ -49,9 +57,47 @@ const LeaderboardScreen = ({processedSleepData}: LeaderboardScreenProps) => {
             />
           </View>
         </View>
-        <ScrollView>
-          {sortedScores.map((user, index) => (
-            <ScoreSlate key={index} username={user.name} score={user.score} />
+        <View style={styles.podiumContainer}>
+          <View style={styles.secondPlace}>
+            <Image
+              source={{uri: sortedScores[1].picture}}
+              style={styles.firstImageContainer}
+            />
+            <Text style={styles.podiumNameStyle}>{sortedScores[1].name}</Text>
+            <Text style={styles.podiumScoreStyle}>
+              {Math.round(sortedScores[1].score)}
+            </Text>
+          </View>
+          <View style={styles.firstPlace}>
+            <Image
+              source={{uri: sortedScores[0].picture}}
+              style={styles.firstImageContainer}
+            />
+            <Text style={styles.podiumNameStyle}>{sortedScores[0].name}</Text>
+            <Text style={styles.podiumScoreStyle}>
+              {Math.round(sortedScores[0].score)}
+            </Text>
+          </View>
+          <View style={styles.thirdPlace}>
+            <Image
+              source={{uri: sortedScores[2].picture}}
+              style={styles.thirdImageContainer}
+            />
+            <Text style={styles.podiumNameStyle}>{sortedScores[2].name}</Text>
+            <Text style={styles.podiumScoreStyle}>
+              {Math.round(sortedScores[2].score)}
+            </Text>
+          </View>
+        </View>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          {sortedScores.slice(3).map((user, index) => (
+            <ScoreSlate
+              key={index}
+              username={user.name}
+              score={user.score}
+              position={index + 4}
+              profilePicture={user.picture}
+            />
           ))}
           <View style={styles.bottomSpacer} />
         </ScrollView>
@@ -82,8 +128,112 @@ const styles = StyleSheet.create({
     shadowRadius: 3.84,
     elevation: 5,
   },
+  podiumContainer: {
+    alignItems: 'center',
+    marginVertical: 20,
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  secondPlace: {
+    justifyContent: 'center',
+    backgroundColor: '#22263C',
+    borderRadius: 8,
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0,
+    padding: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    width: '30%',
+    height: 105,
+    marginTop: 45,
+  },
+  firstPlace: {
+    justifyContent: 'center',
+    backgroundColor: '#252A40',
+    borderRadius: 8,
+    borderBottomRightRadius: 0,
+    borderBottomLeftRadius: 0,
+    padding: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    width: '30%',
+    height: 125,
+    marginTop: 25,
+  },
+  thirdPlace: {
+    justifyContent: 'center',
+    backgroundColor: '#1E2237',
+    borderRadius: 8,
+    borderTopLeftRadius: 0,
+    borderBottomLeftRadius: 0,
+    padding: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 2,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    width: '30%',
+    height: 85,
+    marginTop: 65,
+  },
   bottomSpacer: {
-    height: 100,
+    height: 250,
+  },
+  podiumNameStyle: {
+    color: '#D2D5D9',
+    fontSize: 14,
+    fontFamily: 'Nunito-Bold',
+    fontWeight: '900',
+    textAlign: 'center',
+  },
+  podiumScoreStyle: {
+    color: '#D2D5D9',
+    fontSize: 20,
+    fontFamily: 'Nunito-Bold',
+    fontWeight: '900',
+    textAlign: 'center',
+  },
+  firstImageContainer: {
+    position: 'absolute',
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: '#D2D5D9',
+    alignSelf: 'center',
+    top: -40,
+  },
+  secondImageContainer: {
+    position: 'absolute',
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#D2D5D9',
+    alignSelf: 'center',
+    top: -35,
+  },
+  thirdImageContainer: {
+    position: 'absolute',
+    width: 45,
+    height: 45,
+    borderRadius: 25,
+    backgroundColor: '#D2D5D9',
+    alignSelf: 'center',
+    top: -30,
   },
 });
 

bd872da2bfc4b62e100b1ecb6c52d1a764e393ec
Portego-00 <po93@cornell.edu>
2024-07-31 21:10:08 -0700

    Minor change

--- src/screens/AnalyticsScreen/components/HeartRateSection/HeartRateSection.tsx
+++ src/screens/AnalyticsScreen/components/HeartRateSection/HeartRateSection.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Dimensions, StyleSheet, View} from 'react-native';
+import {Dimensions, StyleSheet, Text, View} from 'react-native';
 import Card from '../../../../components/Card';
 import Header from '../../../../components/Header';
 import {
@@ -13,6 +13,7 @@ import {
 } from '../../../../utils/utils';
 import {LineChart} from 'react-native-chart-kit';
 import {generateLabels} from '../SleepGraphSection/utils';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 type HeartRateSectionProps = {
   day: DateObject;
@@ -43,6 +44,24 @@ const HeartRateSection = ({
   if (hours.length === 0) {
     return null;
   }
+
+  const maxHeartRate = heartRateData.reduce(
+    (max, [, value]) => Math.max(max, value),
+    0,
+  );
+  const minHeartRate = heartRateData.reduce(
+    (min, [, value]) => Math.min(min, value),
+    maxHeartRate,
+  );
+  const averageHeartRate = Math.round(
+    heartRateData.reduce((sum, [, value]) => sum + value, 0) /
+      heartRateData.length,
+  );
+
+  console.log('Max Heart Rate:', maxHeartRate);
+  console.log('Min Heart Rate:', minHeartRate);
+  console.log('Average Heart Rate:', averageHeartRate);
+
   const data = {
     labels: hours.map(hour => hour.toString()),
     datasets: [
@@ -94,6 +113,16 @@ const HeartRateSection = ({
             bezier
           />
         </View>
+        <View style={styles.dataContainer}>
+          <View style={styles.dataTitleContainer}>
+            <Icon name="heart" size={20} color="#eb4b63" />
+            <Text style={styles.dataTitle}>Heart Rate Data</Text>
+          </View>
+          <Text style={styles.dataText}>
+            Range: {minHeartRate} - {maxHeartRate} bpm
+          </Text>
+          <Text style={styles.dataText}>Average: {averageHeartRate} bpm</Text>
+        </View>
       </Card>
     </>
   );
@@ -104,6 +133,25 @@ const styles = StyleSheet.create({
     marginLeft: -30,
     marginTop: 10,
     alignItems: 'center',
+  },
+  dataTitle: {
+    fontFamily: 'Nunito-Bold',
+    fontSize: 18,
+    color: '#eb4b63',
+  },
+  dataTitleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
+  },
+  dataContainer: {
+    marginTop: 20,
+    marginLeft: 20,
+  },
+  dataText: {
+    fontFamily: 'Nunito-Regular',
+    fontSize: 16,
+    color: '#D2D5D9',
   },
 });
 
